8592845cb3bb95bb71c6cc55dcd928b517a9720a
guilhermespicacci <guilhermespicaccilima@gmail.com>
2023-10-13 02:05:56 -0300

    calculadora_0.2

--- script.js
+++ script.js
@@ -3,48 +3,61 @@
  
 
 function CriaCalculadora() {
-    this.inputDisplay = document.getElementById("display");
+    inputDisplay = document.getElementById("display");
     this.inicia = () =>{
-        this.getClick()
-        
+        this.getClick();
+        this.getEnter();
     }
-
+    this.getEnter = () =>{
+      document.addEventListener("keyup", e =>{
+        if (e.keyCode !== 13) return;
+        else{
+          this.fazConta();
+        }
+      })
+    }
     this.getClick = () => {
         document.addEventListener("click",event =>{
         const tag = event.target;
        if (tag.classList.contains("btn-number")) {
-        (insereNumero = () => {
-            
-            
-            let numAdd = tag.innerText;
-            this.inputDisplay.value += numAdd
-            console.log("Numero Adicionado")
-        })();
+        this.insereNumero(tag)
        }
        if(tag.classList.contains("btn-clear")){
-        this.inputDisplay.value = ""
+        this.limpaInput() 
        }
        if (tag.classList.contains("btn-del")) {
-        this.inputDisplay.value = this.inputDisplay.value.slice(0,-1)
+        this.apagaUm()
        }
         if(tag.classList.contains("btn-equal") ){
-            try {
-                const resul = eval(this.inputDisplay.value);
-                if (resul !== undefined && !isNaN(resul)) {
-                  this.inputDisplay.value = resul;
-                } else {
-                  alert("Expressão Inválida");
-                }
-              } catch (e) {
-                alert("Expressão Inválida");
-              }
+           this.fazConta() 
         }
     })
         
 
-    
+    this.insereNumero = (el) => {    
+      let numAdd = el.innerText;
+      inputDisplay.value += numAdd
+      console.log("Numero Adicionado")
+  };
+  this.limpaInput = () => {
+    inputDisplay.value = ""
+  }
+  this.apagaUm = ()=>{
+    inputDisplay.value = this.inputDisplay.value.slice(0,-1)
+  }
+  this.fazConta = () =>{ try {
+    const resul = eval(inputDisplay.value);
+    if (resul !== undefined && !isNaN(resul)) {
+      inputDisplay.value = resul;
+    } else {
+      alert("Expressão Inválida");
+    }
+  } catch (e) {
+    alert("Expressão Inválida");
+  }}
+ } 
 };
-};
+
 
 
 calculadora = new CriaCalculadora
